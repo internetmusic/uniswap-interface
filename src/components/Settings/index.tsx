be2ccde4b57d39637b148a44f20b807db9057c26
Internet Music <thor@internetmusic.ltd.uk>
2020-12-23 14:33:31 +0100

    Test style 350px

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -49,7 +49,7 @@ const StyledMenuButton = styled.button`
   background-color: transparent;
   margin: 0;
   padding: 0;
-  height: 350px;
+  height: 35px;
   background-color: ${({ theme }) => theme.bg3};
 
   padding: 0.15rem 0.5rem;
